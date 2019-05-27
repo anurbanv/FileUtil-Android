33778fab5a8f84d39ed2356b9575854743a8cc21
UrbanAndrius <UrbanAndrius@users.noreply.github.com>
2019-05-27 23:53:22 +0300

    removed final property to allow class extension

--- fileutil/src/main/java/com/andrius/fileutil/FileUtil.java
+++ fileutil/src/main/java/com/andrius/fileutil/FileUtil.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public final class FileUtil {
+public class FileUtil {
 
     private static final String TAG = "FileUtil";
 
